8e2e7e55f0401c0b1bbed09faa7fd2974fd7b4f8
Troy Dutton <troydutton@utexas.edu>
2024-06-24 02:11:59 -0500

    Update project links and add new images

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import mock01 from '../assets/images/mock01.png';
-import mock02 from '../assets/images/mock02.png';
-import mock03 from '../assets/images/mock03.png';
-import mock04 from '../assets/images/mock04.png';
-import mock05 from '../assets/images/mock05.png';
 import mock06 from '../assets/images/mock06.png';
-import mock07 from '../assets/images/mock07.png';
-import mock08 from '../assets/images/mock08.png';
 import mock09 from '../assets/images/mock09.png';
-import mock10 from '../assets/images/mock10.png';
+import recommender from '../assets/images/recommender.png';
+import chess from '../assets/images/chess.png';
+import minecraft from '../assets/images/minecraft.png';
+import rationallama from '../assets/images/rationallama.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -17,34 +13,34 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Conversational Recommender System</h2></a>
+                <a href="https://github.com/UT-ECE364D-D12/CERS" target="_blank" rel="noreferrer"><img src={recommender} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/UT-ECE364D-D12/CERS" target="_blank" rel="noreferrer"><h2>Conversational Recommender System</h2></a>
                 <p>Developing a system which provides contextually informed recommendations by incorporating Large Language Models into traditional recommender systems.</p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><h2>Fashion Atlas</h2></a>
+                <a href="https://github.com/troydutton/fashion-atlas" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/troydutton/fashion-atlas" target="_blank" rel="noreferrer"><h2>Fashion Atlas</h2></a>
                 <p>Created a clothing recommendation app that uses object detection and image encoding to suggest similar styles of clothing.</p>
             </div>
             <div className="project">
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><img src={mock08} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://yujisatojr.itch.io/spacecraft" target="_blank" rel="noreferrer"><h2>Minecraft PVE</h2></a>
-                <p>Leveraged reinforcement learning algorithms to train an agent to defeat hostile creatures within the Minecraft environment.</p>
+                <a href="https://medium.com/@jaspertan_49883/rationallama-fine-tuning-an-llm-for-logical-reasoning-and-why-its-hard-c590ff4081fc" target="_blank" rel="noreferrer"><img src={rationallama} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://medium.com/@jaspertan_49883/rationallama-fine-tuning-an-llm-for-logical-reasoning-and-why-its-hard-c590ff4081fc" target="_blank" rel="noreferrer"><h2>RationaLlama</h2></a>
+                <p>Fine-tuned Llama-2 to solve logical reasoning tasks, outperforming ChatGPT 3.5 Turbo.</p>
             </div>
             <div className="project">
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.datumlearn.com/" target="_blank" rel="noreferrer"><h2>Chess Engine</h2></a>
+                <a href="https://github.com/troydutton/Chess" target="_blank" rel="noreferrer"><img src={chess} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/troydutton/Chess" target="_blank" rel="noreferrer"><h2>Chess Engine</h2></a>
                 <p>Developed a neural-network based chess engine featuring a Monte-Carlo Tree Search algorithim inspired by DeepMind's AlphaZero paper.</p>
             </div>
             <div className="project">
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><h2>ASL Identifer</h2></a>
-                <p>Designed a real-time American Sign Language classifier which accurately matches hand gestures to their respective letters.</p>
+                <a href="https://github.com/CashelF/rl-minecraft-pvp" target="_blank" rel="noreferrer"><img src={minecraft} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/CashelF/rl-minecraft-pvp" target="_blank" rel="noreferrer"><h2>Minecraft PVE</h2></a>
+                <p>Leveraged reinforcement learning algorithms to train an agent to defeat hostile creatures within the Minecraft environment.</p>
             </div>
             <div className="project">
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><h2>Wordle Solver</h2></a>
-                <p>Devised an algorithm which suggests optimal wordle guesses by analyzing the user gess history using information theory..</p>
+                <a href="https://github.com/troydutton/Asli" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/troydutton/Asli" target="_blank" rel="noreferrer"><h2>ASL Identifer</h2></a>
+                <p>Designed a real-time American Sign Language classifier which accurately matches hand gestures to their respective letters.</p>
             </div>
         </div>
     </div>
