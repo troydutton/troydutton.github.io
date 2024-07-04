a5734838c82de9744511456124d49de7f5a2f504
Troy Dutton <troydutton@utexas.edu>
2024-07-04 14:06:47 -0500

    Update resume, Add resume link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Article from '@mui/icons-material/Article';
 import '../assets/styles/Footer.scss'
 
 function Footer() {
@@ -9,6 +10,7 @@ function Footer() {
       <div>
         <a href="https://github.com/troydutton" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/troydutton/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://drive.google.com/file/d/1U5UcZ-s5frRiwt9-r2NJQBc76sCFD_ej/view?usp=sharing" target="_blank" rel="noreferrer"><Article/></a>
       </div>
       <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
     </footer>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -17,7 +17,7 @@ function Main() {
           <div className="social_icons">
             <a href="https://github.com/troydutton" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/troydutton/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-            <a href="https://drive.google.com/file/d/1RxmeBxGNjLX551qefuM3bIq-LfHGxWzT/view?usp=sharing" target="_blank" rel="noreferrer"><Article/></a>
+            <a href="https://drive.google.com/file/d/1U5UcZ-s5frRiwt9-r2NJQBc76sCFD_ej/view?usp=sharing" target="_blank" rel="noreferrer"><Article/></a>
           </div>
           <h1>Troy Dutton</h1>
           <p>Student | Software Engineer | AI Researcher</p>
@@ -25,7 +25,7 @@ function Main() {
           <div className="mobile_social_icons">
             <a href="https://github.com/troydutton" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/troydutton/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-            <a href="https://drive.google.com/file/d/1aIeOv2HabcNdOkk9T0bygqJLd1I9VZgp/view?usp=sharing" target="_blank" rel="noreferrer"><Article/></a>
+            <a href="https://drive.google.com/file/d/1U5UcZ-s5frRiwt9-r2NJQBc76sCFD_ej/view?usp=sharing" target="_blank" rel="noreferrer"><Article/></a>
           </div>
         </div>
       </div>
